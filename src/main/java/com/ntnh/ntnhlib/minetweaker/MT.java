a16dedd0e22c6ed0385e6ddbc5646d97660c8b57
Bufka Second <4129261@school.ecsd.net>
2026-01-14 15:54:27 +0000

    spotlessApply

--- src/main/java/com/ntnh/ntnhlib/minetweaker/MT.java
+++ src/main/java/com/ntnh/ntnhlib/minetweaker/MT.java
@@ -1,5 +1,10 @@
 package com.ntnh.ntnhlib.minetweaker;
 
+import java.io.IOException;
+import java.util.Set;
+
+import net.minecraft.nbt.NBTTagCompound;
+
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import com.google.common.reflect.ClassPath;
@@ -7,16 +12,12 @@ import com.google.common.reflect.ClassPath;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.event.FMLInterModComms;
 
-import net.minecraft.nbt.NBTTagCompound;
-
-import java.io.IOException;
-import java.util.Set;
-
 public class MT {
+
     public static void loadMineTweakerScripts() {
         if (Loader.isModLoaded("MineTweaker3")) {
             try {
-                ClassLoader classLoader = MT.class.getClassLoader();  // Use MT.class here
+                ClassLoader classLoader = MT.class.getClassLoader(); // Use MT.class here
                 ClassPath classPath = ClassPath.from(classLoader);
                 Set<ClassPath.ResourceInfo> resources = classPath.getResources();
 
@@ -25,19 +26,20 @@ public class MT {
                     if (path.startsWith("minetweaker/") && path.endsWith(".zs")) {
                         try {
                             String data = Resources.toString(resource.url(), Charsets.UTF_8);
-                            String scriptName = path.substring("minetweaker/".length());  // Use relative path as name to avoid conflicts
+                            String scriptName = path.substring("minetweaker/".length()); // Use relative path as name to
+                                                                                         // avoid conflicts
 
                             NBTTagCompound nbtData = new NBTTagCompound();
                             nbtData.setString("name", scriptName);
                             nbtData.setString("content", data);
                             FMLInterModComms.sendMessage("MineTweaker3", "addMineTweakerScript", nbtData);
                         } catch (IOException ex) {
-                            ex.printStackTrace();  // Handle individual file errors without stopping
+                            ex.printStackTrace(); // Handle individual file errors without stopping
                         }
                     }
                 }
             } catch (IOException ex) {
-                ex.printStackTrace();  // For ClassPath initialization errors
+                ex.printStackTrace(); // For ClassPath initialization errors
             }
         }
     }
